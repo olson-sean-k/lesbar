55f903b4becf060ff2f4de79b8e479dd278f7017
Sean Olson <olson.sean.k@gmail.com>
2025-02-09 18:27:53 -0800

    Consider private-use characters in `StrExt::has_printable_text`.

--- lesbar-text/src/lib.rs
+++ lesbar-text/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
+use unicode_properties::{GeneralCategory, UnicodeGeneralCategory};
 use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
 
@@ -12,13 +13,6 @@ impl StrExt for str {
     //       This must be as well-defined as possible and documented accordingly. This function
     //       implements this important predicate and so should probably provide this definition in
     //       its API documentation.
-    // TODO: Consider particular Unicode code points that may thwart this implementation or present
-    //       some ambiguity. For example, private-use code points report zero-width in
-    //       `unicode-width` and have no established properties, because they are user-defined. Are
-    //       these code points and grapheme clusters "printable"? Note that software tends to
-    //       render these with the replacement glyph (which is distinct from the replacement
-    //       **character**). U+E064 renders a replacement glyph in GNOME Terminal and Termius, for
-    //       example.
     fn has_printable_text(&self) -> bool {
         self.width() != 0
             && self
@@ -36,10 +30,58 @@ impl StrExt for str {
 struct Grapheme<'t>(&'t str);
 
 impl Grapheme<'_> {
+    fn as_singular_code_point(&self) -> Option<char> {
+        match self
+            .0
+            .chars()
+            .enumerate()
+            .last()
+            .expect("grapheme cluster has no code points")
+        {
+            (0, point) => Some(point),
+            _ => None,
+        }
+    }
+
+    // A grapheme cluster is considered printable if it is a private-use character or its display
+    // width is non-zero per UCS and UAX11. Private-use characters have no display width, but are
+    // typically rendered as a replacement glyph when not assigned or recognized. When used as
+    // intended, they represent a glyph with some non-zero rendered width.
     pub fn is_printable_text(&self) -> bool {
-        self.0.width() != 0
+        self.is_private_use_character() || self.0.width() != 0
+    }
+
+    fn is_private_use_character(&self) -> bool {
+        self.as_singular_code_point()
+            .map(UnicodeGeneralCategory::general_category)
+            .map_or(false, |category| {
+                matches!(category, GeneralCategory::PrivateUse)
+            })
     }
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    extern crate alloc;
+    extern crate std;
+
+    use rstest::rstest;
+
+    use crate::StrExt as _;
+
+    #[rstest]
+    fn empty_str_has_no_printable_text() {
+        assert!(!"".has_printable_text());
+    }
+
+    #[rstest]
+    #[case::one_plane_basic_multilingual("\u{E064}")]
+    #[case::one_plane_15("\u{F00FF}")]
+    #[case::one_plane_16("\u{100000}")]
+    #[case::one_with_non_printable("\u{200B}\u{E064}")]
+    #[case::many_from_each_plane("\u{E000}\u{F0000}\u{10FFFD}")]
+    #[case::many_with_non_printable("\u{E000}\u{200B}\u{E001}")]
+    fn str_with_private_use_chars_has_printable_text(#[case] text: &str) {
+        assert!(text.has_printable_text())
+    }
+}
